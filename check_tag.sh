beb47bac4d1cf2713b9b6ff667296d146e1f1ddf
Sönke Liebau <soenke.liebau@stackable.de>
2022-06-28 11:21:05 +0200

    Syntax errors in action file

--- check_tag.sh
+++ check_tag.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+echo Checking $1
 [[ "$1" =~ ^kafka|zookeeper|nifi|druid|opa|hbase|hdfs|trino|airflow|superset|spark-k8s|.+-stackable\d+\.\d+\.\d+ ]] && echo "Tag valid"  || exit 255
